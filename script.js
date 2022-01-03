22bdfbcabe89a7badd5d23ab489d9a7ed5b3b256
Miguel Vasquez <vasquezdevelopment@gmail.com>
2022-01-02 21:36:36 -0500

    adding code

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    var menu = document.getElementById("menu-container");
+    menu.classList.toggle("show");
+}
+
+let btn = document.querySelector(".toggle");
+let icon = document.querySelector(".fa-bars");
+
+
+btn.onclick = function(){
+    if(icon.classList.contains("fa-bars")){
+        icon.classList.replace("fa-bars","fa-times");
+    }else{
+        icon.classList.replace("fa-times","fa-bars");
+    }
+}
